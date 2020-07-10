b827ab1e8157e91c46c6022826daef9241fc53db
Dane Mancuso <danegmancuso@gmail.com>
2020-07-10 01:01:16 +0000

    clean up old comments and console.logs

--- express_server.js
+++ express_server.js
@@ -6,11 +6,9 @@ app.set("view engine", "ejs");
 
 // Parsers - this has to come before all routes.
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt');
 const cookieSession = require('cookie-session');
 const saltRounds = 10;
-
 
 
 app.use(cookieSession({
@@ -18,33 +16,17 @@ app.use(cookieSession({
   keys: ['key1', 'key2']
 }));
 
-app.use(cookieParser())
-
-// req.session["user_id"]
-// req.session["user_id"]
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-// custom middleware 
-// app.use((req, res, next) => {
-//   let currentUserId = req.session["user_id"];
-//   // console.log(currentUserId)
-// next();
-// });
+
 
 // DATABASE >>>------------------------>
-
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
 
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
-
 
 const users = { 
   "userRandomID": {
@@ -85,7 +67,6 @@ function authenticateUser(email, password) {
 function checkEmailDupe(emailInput) {
   for (user in users) {
     if (emailInput === users[user].email) {
-      // console.log(users[user])
       return true;
     }
   }
@@ -138,14 +119,12 @@ app.post("/login", (req, res) => {
   // fetch login_user
 app.get("/login", (req, res) => {
   let templateVars = { urls: urlDatabase, user: users[req.session["user_id"]] };
-  console.log('req session', users[req.session["user_id"]])
   res.render("login_user", templateVars);
 });
 
 
 //remove cookie from username
 app.post("/logout",(req, res) => {
- 
   req.session = null;
   res.redirect("/urls/")
 });
@@ -153,7 +132,6 @@ app.post("/logout",(req, res) => {
 //fetches register page
 app.get('/register', (req,res) => {
   let templateVars = { user: users[req.session["user_id"]] }
-  // let templateVars = { user: req.session["user_id"] }
   res.render("register_user", templateVars)
 });
 
@@ -162,7 +140,7 @@ app.post('/register', (req, res) => {
   const id = generateRandomString();
   const email = req.body.email;
   const password = req.body.password;
-  // 404 res if input fields empty
+
   if (email === "" || password === "") {
     res.status(400).send('Error: empty user input fields')
   } else if (checkEmailDupe(email)) {
@@ -174,7 +152,6 @@ app.post('/register', (req, res) => {
     password: bcrypt.hashSync(password, saltRounds)
   }
   users[id] = newUser;
-
   req.session['user_id'] = id;
   res.redirect("/urls/")
  }
@@ -189,7 +166,6 @@ app.get("/urls", (req, res) => {
     userIds: Object.keys(users),
     usrUrls: urlsForUser(req.session["user_id"])
     }; 
-    console.log(urlsForUser(req.session["user_id"]))
   //ejs: can only send variables as objects
   res.render("urls_index", templateVars);
 });
@@ -210,15 +186,11 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: urlDatabase[req.params.shortURL].longURL, 
     user: users[req.session["user_id"]]
   };
-  // console.log(req.params.shortURL)
-  // console.log(urlDatabase[req.params.shortURL].longURL)
   res.render("urls_show", templateVars);
 });
 
 //Add a POST request to update/edit a resource. redirects to same page. 
 app.post("/urls/:shortURL", (req, res) => {
-  // urlDatabase[req.params.shortURL] = req.body.longURL;
-  // urlsForUser(req.session["user_id"]).shortURL = req.body.longURL
   urlDatabase[req.params.shortURL] = {
     longURL: req.body.longURL,
     userID: req.session["user_id"]
@@ -229,7 +201,6 @@ app.post("/urls/:shortURL", (req, res) => {
 
 app.post("/urls", (req, res) => {
   let id = generateRandomString();
-  // urlDatabase[id] = req.body.longURL;
   urlDatabase[id] = {longURL: req.body.longURL, userID: users[req.session["user_id"]]['id']}
   res.redirect(`/urls/${id}`);
 });
@@ -243,7 +214,6 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 // redirect to actual long link page
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
-  // console.log(longURL)
   res.redirect(longURL);
 
 });
